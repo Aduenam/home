47d5533ee2e169b5c43e06f038686b2919204689
Kamille Tipan <kamille.tipan@gmail.com>
2018-08-21 17:19:08 +0000

    fixed news pages

--- js/newsAPI.js
+++ js/newsAPI.js
@@ -6,6 +6,8 @@ var url = 'https://newsapi.org/v2/everything?' +
           'apiKey=2f002fab97254fc48292f9444d1d12a6';
           
 var slideshow_element = document.getElementById("news-slideshow");
+var default_image = "https://cdn3.bigcommerce.com/s-vw57mn/products/40160/images/117175/Hero-Arts-Cling-Stamps-Newspaper-Bold-Prints-857009156494_image1__21010.1517348776.600.600.jpg?c=2";
+var on_err = 'this.src="'+default_image+'"';
 
 var i = 1;
 var req = new Request(url);
@@ -20,59 +22,38 @@ fetch(req)
             var website = article.url;
             var text = article.description;
             if (newImage == null) {
-                newImage = "https://cdn3.bigcommerce.com/s-vw57mn/products/40160/images/117175/Hero-Arts-Cling-Stamps-Newspaper-Bold-Prints-857009156494_image1__21010.1517348776.600.600.jpg?c=2";
+                newImage = default_image;
             }
             
             var div = document.createElement('div');
-            div.className = 'mySlides fade';
-            div.innerHTML =
-             '<div class="numbertext">'+ i + ' / 20</div>\
-                <a href = ' + website + '> <img src=' + newImage + ' style="width:100%"></a>\
-                 <div class="text">' + text +'</div>'; 
+            if (i == 5) {div.className = 'carousel-item active';}
+            else {div.className = 'carousel-item';}
+            
+            div.style = 'background-color:transparent;'
+            
+            var a = document.createElement('a');
+            a.setAttribute('href', website);
+            
+            var carousel_item_div = document.createElement('div');
+            carousel_item_div.className = "carousel-caption d-md-block";
+            carousel_item_div.innerHTML = '<h5 class="captionstyles">' + article.title + '</h5>';
+            
+            var img = document.createElement('img');
+            img.setAttribute('src', newImage);
+            img.setAttribute('onerror', on_err);
+            img.setAttribute('style', 'width: 100%');
+            
+            a.appendChild(img);
+            a.appendChild(carousel_item_div);
+            
+            div.appendChild(a);
                  
             slideshow_element.appendChild(div);
+            
             i +=1;
 
         });
         
-        showSlides();
-        
-        
-    });
-
-
-
-// var lang = new Array();
-// lang = ["English", "Chinese", "Korean"];
-
-// function buildLanguages() {
-//     var langDiv = document.getElementById("langDiv");
-//     var html = "<form>";
-//     for (var i = 0; i < lang.length; i++) {
-//         html += "<input type='checkbox' name='" + lang[i] + "' value='" + i + "' onClick=\"setValue(this.value);\">" + lang[i] + "<br>";
-//     }
-//     html += "</form>";
-
-//     langDiv.innerHTML = html;
-// }
-// buildLanguages();
-    
-
-var slideIndex = 0;
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block"; 
-    
-    setTimeout(showSlides, 1000); // Change image every 6 seconds
-    sleep(5000);
-}
-
 
    
+    });
